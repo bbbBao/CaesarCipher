7d977c9000c16bd041472972eaac5bc4aa1e6ba5
Tianyi Bao <tianyib831@gmail.com>
2022-10-06 23:47:17 -0400

    Encryption function fix

--- app/src/main/java/com/example/caesarcipher/MainActivity.java
+++ app/src/main/java/com/example/caesarcipher/MainActivity.java
@@ -30,24 +30,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public static StringBuffer encrypt(String text, int s)
-    {
+    public static StringBuffer encrypt(String text, int s) {
         StringBuffer result= new StringBuffer();
-
-        for (int i=0; i<text.length(); i++)
-        {
-            if (Character.isUpperCase(text.charAt(i)))
-            {
-                char ch = (char)(((int)text.charAt(i) +
-                        s - 65) % 26 + 65);
-                result.append(ch);
-            }
-            else
-            {
-                char ch = (char)(((int)text.charAt(i) +
-                        s - 97) % 26 + 97);
-                result.append(ch);
-            }
+        for (int i=0; i<text.length(); i++) {
+            char ch = (char)(((int)text.charAt(i) + s - 97) % 26 + 97);
+            result.append(ch);
         }
         return result;
     }
